32e81ea3e7120eaa8ea8c04d7d185344af11724d
Jondic145 <78202371+Jondic145@users.noreply.github.com>
2022-11-01 14:06:03 +0700

    first commit

---
+++ database/migrations/2022_10_31_161121_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('nama_pegawai', 255 );
+            $table->jabatan('');
+            $table->tanggal_masuk();
+            $table->status();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
